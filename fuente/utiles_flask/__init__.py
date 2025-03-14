8ac4cc5610fcad55ffb382660d99f055966454e9
Herni <hernanatn@gmail.com>
2025-03-14 13:44:45 -0300

    [AG]: commit inicial

---
+++ fuente/utiles_flask/__init__.py
@@ -0,0 +1,20 @@
+"""
+===============
+chastack-utiles-flask
+===============
+Librería de útiles para flask en Python 3.
+
+* Repositorio del proyecto: https://github.com/Hernanatn/chastack/utiles_flask
+
+Derechos de autor (c) 2025 Ch'aska SRL. Distribuído bajo licencia MIT.
+Autores:
+ - Hernan ATN | herni@cajadeideas.ar 
+"""
+
+import utiles_flask.imagenes
+import utiles_flask.ruteo
+import utiles_flask.texto
+
+
+if __name__ == '__main__':
+    print(__doc__)
