7349fd232052e76da1e0edadd2f388977f59b183
Herni <hernanatn@gmail.com>
2025-03-17 01:00:10 -0300

    [DIST] v0.1.4

--- fuente/setup.py
+++ fuente/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='chastack-utiles-flask',
     description="setuptools.build_meta",
-    version='0.1.1',
+    version='0.1.4',
     author           = 'Hernán A. Teszkiewicz Novick',
     author_email     = 'herni@cajadeideas.ar',
     url= 'https://github.com/Hernanatn/utiles_google',
