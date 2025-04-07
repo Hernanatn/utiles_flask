c0178d929fde70482a088dde07af459ccd5c9a6b
Herni <hernanatn@gmail.com>
2025-04-07 19:47:49 -0300

    [AG] constructor de <img> responsivo

--- fuente/chastack_utiles_flask/imagenes.py
+++ fuente/chastack_utiles_flask/imagenes.py
@@ -40,3 +40,47 @@ def guardarImagen(imagen : FileStorage,nombre_archivo : str, directorio : str,*,
     imagen_50px.save(os.path.join(directorio,subdirectorio,f"50_{nombre_archivo_imagen}"), quality=30 )
 
     return nombre_archivo_imagen
+
+
+from flask import Flask
+from functools import cache
+
+@cache
+def imagenResponsiva(
+    nombre_archivo: str,
+    servidor: Flask,
+    alt: Optional[str] = '',
+    clases: Optional[str] = ''
+) -> str:
+    """
+    Devuelve una etiqueta <img> HTML con srcset para versiones responsivas de una imagen'.
+
+    Parámetros:
+        :param str nombre_archivo: La ruta relativa al archivo desde 'estatico', incluyendo la extensión (ej. 'imagenes/libros/portada.webp').
+        :param Flask servidor: Instancia de la app Flask.
+        :param str alt: Texto alternativo.
+        :param Opcional[str] clases: Clases CSS opcionales.
+
+    Devuelve:
+        :param str <img>: 
+    """
+    tamaños = [50, 100, 200, 500, 750, 1000, 2000]
+
+    with servidor.app_context():
+        def ruta(tamaño: int) -> str:
+            return servidor.url_for('static', filename=f"/{tamaño}_{nombre_archivo}")
+
+        srcset = ", ".join(f"{ruta(t)} {t}w" for t in tamaños)
+        src = ruta(200)
+    sizes = """
+        (max-width: 30em) 100vw,
+        (max-width: 40em) 90vw,
+        (max-width: 50em) 80vw,
+        (max-width: 60em) 60vw,
+        (max-width: 80em) 40vw,
+        (max-width: 100em) 33vw,
+        25vw"""
+
+    etiqueta_img = f'<img src="{src}" srcset="{srcset}" sizes="{sizes}" alt="{alt}" loading="lazy" {f'class="{clases}"' if clases else ''}/>'
+
+    return etiqueta_img
